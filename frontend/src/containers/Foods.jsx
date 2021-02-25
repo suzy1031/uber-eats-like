24ca2f28f92e2f8969e7bbdccdc52012a57c605e
koheiSugawara <59728817+koheiSugawara@users.noreply.github.com>
2021-02-26 07:37:52 +0900

    second commit

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,18 +1,146 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useReducer, useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+// reducer
+import { initialState as foodsInitialState, foodsActionTypes, foodsReducer } from '../reducers/foods';
 
 // apis
 import { fetchFoods } from '../apis/foods';
 
+// constants
+import { COLORS } from '../style_constants';
+import { REQUEST_STATE } from '../constants';
+
+// components
+import { LocalMallIcon } from '../components/Icons';
+import { FoodWrapper } from '../components/FoodWrapper';
+import Skeleton from '@material-ui/lab/Skeleton';
+import { FoodOrderDialog } from '../components/FoodOrderDialog';
+
+// images
+import MainLogo from '../images/logo.png';
+import FoodImage from '../images/food-image.jpg';
+
 export const Foods = ({ match }) => {
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
+  // stateオブジェクト
+  const initialState = {
+    isOpenOrderDialog: false,
+    selectedFood: null,
+    selectedFoodCount: 1,
+  }
+
+  // initialStateをuseStateに渡して初期値とする
+  const [state, setState] = useState(initialState);
+
   useEffect(() => {
+    dispatch({type: foodsActionTypes.FETCHING });
     fetchFoods(match.params.restaurantsId)
-      .then((data) =>
-        console.log(data)
-      )
-  },[])
+    .then((data) => {
+      dispatch({
+        type: foodsActionTypes.FETCH_SUCCESS,
+        payload: {
+          foods: data.foods
+        }
+      });
+    })
+  }, [])
+
+  const submitOrder = () => {
+    console.log('登録ボタンが押された!')
+  }
   return (
     <Fragment>
-      フード一覧
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <BagIconWrapper>
+          <Link to="orders">
+            <ColoredBagIcon fontSize="large" />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <FoodsList>
+        {
+          foodsState.fetchState === REQUEST_STATE.LOADING ?
+            <Fragment>
+              {
+                [...Array(12).keys()].map(i =>
+                  <ItemWrapper key={i}>
+                    <Skeleton key={i} variant="rect" width={450} height={180} />
+                  </ItemWrapper>
+                )
+              }
+            </Fragment>
+          :
+          foodsState.foodsList.map(food =>
+            <ItemWrapper key={food.id}>
+              <FoodWrapper
+                food={food}
+                onClickFoodWrapper={(food) => setState({
+                  ...state,
+                  isOpenOrderDialog: true,
+                  selectedFood: food
+                })}
+                imageUrl={FoodImage}/>
+            </ItemWrapper>
+          )
+        }
+      </FoodsList>
+      {
+        state.isOpenOrderDialog &&
+        <FoodOrderDialog
+          food={state.selectedFood}
+          countNumber={state.selectedFoodCount}
+          onClickCountUp={() => setState({
+            ...state,
+            selectedFoodCount: state.selectedFoodCount + 1,
+          })}
+          onClickCountDown={() => setState({
+            ...state,
+            selectedFoodCount: state.selectedFoodCount - 1,
+          })}
+          onClickOrder={() => submitOrder()}
+          isOpen={state.isOpenOrderDialog}
+          onClose={() => setState({
+            ...state,
+            isOpenOrderDialog: false,
+            selectedFood: null,
+            selectedFoodCount: 1,
+          })}
+        />
+      }
     </Fragment>
   )
 }
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+`;
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+`;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+const FoodsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+`;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+`;
